8bbc8405c168fdd1152bb38b6759bec2f19a4651
juanj.torres <juanj.torres@LSTKCO14817.stkproj.com>
2020-02-25 22:08:32 +0100

    Fix registrar proyecto test

--- test/Promotores_test.js
+++ test/Promotores_test.js
@@ -1,10 +1,10 @@
-const Promotores = artifacts.require("Promotores");
+const PlataformaPromoInver = artifacts.require("PlataformaPromoInver");
 
 contract('Promotores', function (accounts) {
     console.log(accounts);
     
     beforeEach(async function () {
-        this.promotores = await Promotores.new();
+        this.plataformaPromoInver = await PlataformaPromoInver.new();
     });
     
     it('Registrar un proyecto', async function () {
@@ -14,17 +14,45 @@ contract('Promotores', function (accounts) {
         const cuentaPromotor = accounts[0];
         const cuentaProyecto = accounts[1];
 
-       await this.promotores.registrarPromotor("Promotor 1", "B123012", 10000, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
+       await this.plataformaPromoInver.registrarPromotor("Promotor 1", "B123012", 10000, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
             .on('receipt', function(receipt){
 
                 assert.equal(receipt.logs[0].event, "PromotorRegistrado");            
             });
 
-        await this.promotores.registrarProyecto(cuentaProyecto, "Proyecto 1", 0, 0, 200, 10, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
+        await this.plataformaPromoInver.registrarProyecto(cuentaProyecto, "Proyecto 1", 0, 0, 200, 10, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
             .on('receipt', function(receipt){
-
+                console.log(JSON.stringify(receipt, null, 2));
                 assert.equal(receipt.logs[0].event, "ProyectoRegistrado");            
             });            
+    });
+
+    it('Registrar un proyecto con tokenGoal superior a capacidad promotor', async function () {
+        
+        console.log(accounts[0]);
+
+        const cuentaPromotor = accounts[0];
+        const cuentaProyecto = accounts[1];
+
+       await this.plataformaPromoInver.registrarPromotor("Promotor 1", "B123012", 10000, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
+            .on('receipt', function(receipt){
+
+                assert.equal(receipt.logs[0].event, "PromotorRegistrado");            
+            });
+
+        try {
+            await this.plataformaPromoInver.registrarProyecto(cuentaProyecto, "Proyecto 1", 0, 0, 20000, 10, { from: cuentaPromotor, gasPrice: 1, gas: 3000000 })
+                .on('receipt', function(receipt){
+                    console.log(JSON.stringify(receipt, null, 2));
+                    assert.equal(receipt.logs[0].event, "ProyectoRegistrado");            
+                });   
+        } catch (error) {
+            console.log("Error proyecto: " + error.message);
+            return;
+        }
+
+        assert.fail('Proyectos: Expected throw not received');
+          
     });
     
     
